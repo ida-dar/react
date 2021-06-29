2126d4a56925e351104d17c817f09dfc35e1c86b
Ida Darmochwał <ida.mdart@gmail.com>
2021-06-29 19:31:01 +0200

    Enable adding columns

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
-import Column from '../Column/Column.js';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
-        <List title={[ 'Things to do ', <sup key='1'>soon!</sup> ]} image='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        {/* <List {...listData} title={[ 'Things to do ', <sup key='1'>soon!</sup> ]} image='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'>
           <p>I'm planning on doing all these things sonner, rather than later!</p>
-        </List>
+        </List> */}
+        <List {...listData} />
       </main>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,34 +1,69 @@
 import React from 'react';
 import styles from './List.scss';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
+import Creator from '../Creator/Creator.js';
 import PropTypes from 'prop-types';
 
 class List extends React.Component { //! class component should always start with propTypes.
+  state ={
+    columns: this.props.columns || [], // if this.props.columns has not been defined, i.e. the component has not received props columns, then this.state.columns will contain an empty array []
+  }
+
   static propTypes = {  // property in which we save type definitions must be called 'propTypes' in which we save an object. Keys in the object are components' properties. For each property name, we provide its type, using the types saved in the imported PropTypes object.
     title: PropTypes.node.isRequired, // 'isRequired' displays an error in console if the component doesn't receive the propperty. Without 'isRequired' the propperty is optional.
     image: PropTypes.string,
-    children: PropTypes.node,
+    description: PropTypes.node,
+    columns: PropTypes.array,
   }
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>, // default property value. If no content for list description is given, then the default decription is inserted.
+    description: settings.defaultListDescription, // default property value. If no content for list description is given, then the default decription is inserted.
   }
 
   render() {
+    //console.log(this.state);
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} imageSrc={this.props.image} />
+        <Hero
+        titleText={this.props.title}
+        imageSrc={this.props.image}
+        />
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-          <Column title='Garden' />
-          <Column title='Home' />
-          <Column title='Shopping' />
+        {this.state.columns.map(({key, ...columnProps}) => (
+          // In this case (in a loop or .map method if we generate a component for each element in the array), we have to give each column the key. It has to be done explicitly - i.e. in the JSX code there must be expresion 'key = {}'.
+          <Column key={key} {...columnProps} />
+        ))}
+        </div>
+        <div className={styles.creator}>
+          {/* this function does not change the meaning of 'this', so it still points to an instance of the class. If we insert action={this.addColumn}, then it points to addColumn() */}
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+          {/* another option: */}
+          {/* <Creator text={settings.columnCreatorText} action={this.addColumn.bind(this)}/> */}
         </div>
       </section>
     )
+  }
+
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
   }
 }
 
