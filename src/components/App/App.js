2dc3541c0a4378ab0e310d6e30e54ed4eb1ac077
Ida Darmochwał <ida.mdart@gmail.com>
2021-06-29 17:40:27 +0200

    Add Column component & modify Hero and List

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
+import Column from '../Column/Column.js';
 
 class App extends React.Component {
   render() {
@@ -8,7 +9,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <List />
+        <List title={[ 'Things to do ', <sup key='1'>soon!</sup> ]} image='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'>
+          <p>I'm planning on doing all these things sonner, rather than later!</p>
+        </List>
       </main>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import PropTypes from 'prop-types';
 
-class List extends React.Component {
+class List extends React.Component { //! class component should always start with propTypes.
+  static propTypes = {  // property in which we save type definitions must be called 'propTypes' in which we save an object. Keys in the object are components' properties. For each property name, we provide its type, using the types saved in the imported PropTypes object.
+    title: PropTypes.node.isRequired, // 'isRequired' displays an error in console if the component doesn't receive the propperty. Without 'isRequired' the propperty is optional.
+    image: PropTypes.string,
+    children: PropTypes.node,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>, // default property value. If no content for list description is given, then the default decription is inserted.
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} imageSrc={this.props.image} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column title='Garden' />
+          <Column title='Home' />
+          <Column title='Shopping' />
+        </div>
       </section>
     )
   }
