4ba6e7949e3bd0dc535d07c7475a3512a6790fa0
Ida Darmochwał <ida.mdart@gmail.com>
2021-07-05 13:42:30 +0200

    Store setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
 
